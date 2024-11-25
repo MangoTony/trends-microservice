f5512a84a7a8401c4db4d6a13923dc1a1fe12042
MangoTony <tony@finmango.org>
2024-11-25 13:00:06 -0800

    changed data -> trendsData

--- trends_microservice/app.py
+++ trends_microservice/app.py
@@ -35,7 +35,7 @@ def search_terms():
    
     # Return the parsed parameters as a JSON response (for demo purposes)
     response = jsonify({
-        'data': data
+        'trendsData': data
     })
 
     response.headers.add('Access-Control-Allow-Origin', '*')
